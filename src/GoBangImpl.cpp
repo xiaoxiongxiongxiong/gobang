03eae63998468721f7968b0d7756a757bb6e2777
xiaoxiongxiongxiong <981303787@qq.com>
2021-05-25 22:26:49 +0800

    初步实现五子棋规则部分功能

---
+++ src/GoBangImpl.cpp
@@ -0,0 +1,202 @@
+﻿#include "GoBangImpl.h"
+#include <string.h>
+
+bool CGoBangImpl::init()
+{
+    for (int i = 0; i < GOBANG_BOARD_GRID_MAX; i++)
+    {
+        for (int j = 0; j < GOBANG_BOARD_GRID_MAX; j++)
+        {
+            _board_info[i][j].x = i;
+            _board_info[i][j].y = j;
+            _board_info[i][j].used = false;
+            _board_info[i][j].type = GOBANG_PIECE_NONE;
+        }
+    }
+
+    return true;
+}
+
+void CGoBangImpl::uninit()
+{
+
+}
+
+bool CGoBangImpl::getBoardInfo(gobang_board_info_t * board_info[GOBANG_BOARD_GRID_MAX])
+{
+    memcpy(board_info, &_board_info, sizeof(_board_info[0]) * GOBANG_BOARD_GRID_MAX);
+    return true;
+}
+
+bool CGoBangImpl::pieceDown(const int x, const int y, GOBANG_PIECE_TYPE type)
+{
+    if (checkPieceDown(x, y, type))
+    {
+        _board_info[x][y].type = type;
+        _board_info[x][y].used = true;
+        return true;
+    }
+
+    return false;
+}
+
+bool CGoBangImpl::checkPieceDown(const int x, const int y, GOBANG_PIECE_TYPE type)
+{
+    if (x < 0 || x >= GOBANG_BOARD_GRID_MAX ||
+        y < 0 || y >= GOBANG_BOARD_GRID_MAX ||
+        (GOBANG_PIECE_WHITE != type && GOBANG_PIECE_BLACK != type))
+    {
+        return false;
+    }
+
+    return !_board_info[y][x].used;
+}
+
+bool CGoBangImpl::checkGameOver(const int x, const int y, GOBANG_PIECE_TYPE type)
+{
+    if (getTopPiecesCount(x, y, type) + getBottomPiecesCount(x, y, type) >= 6)
+        return true;
+
+    if (getTopLeftPiecesCount(x, y, type) + getBottomRightPiecesCount(x, y, type) >= 6)
+        return true;
+
+    if (getTopRightPiecesCount(x, y, type) + getBottomLeftPiecesCount(x, y, type) >= 6)
+        return true;
+
+    if (getLeftPiecesCount(x, y, type) + getRightPiecesCount(x, y, type) >= 6)
+        return true;
+
+    return false;
+}
+
+uint16_t CGoBangImpl::getTopPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (y >= 0)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        y--;
+    }
+
+    return count;
+}
+
+uint16_t CGoBangImpl::getTopLeftPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (y >= 0 && x >= 0)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        x--;
+        y--;
+    }
+
+    return count;
+}
+
+uint16_t CGoBangImpl::getTopRightPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (y >= 0 && x <= 14)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        y--;
+        x++;
+    }
+
+    return count;
+}
+
+uint16_t CGoBangImpl::getLeftPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (x >= 0)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        x--;
+    }
+
+    return count;
+}
+
+uint16_t CGoBangImpl::getRightPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (x <= 14)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        x++;
+    }
+
+    return count;
+}
+
+uint16_t CGoBangImpl::getBottomPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (y <= 14)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        y++;
+    }
+
+    return count;
+}
+
+uint16_t CGoBangImpl::getBottomLeftPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t uiCount = 0;
+    while (y <= 14 && x >= 0)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        uiCount++;
+        x--;
+        y++;
+    }
+
+    return uiCount;
+}
+
+uint16_t CGoBangImpl::getBottomRightPiecesCount(int x, int y, GOBANG_PIECE_TYPE type)
+{
+    uint16_t count = 0;
+    while (y <= 14 && x <= 14)
+    {
+        if (!_board_info[x][y].used)
+            break;
+        if (_board_info[x][y].type != type)
+            break;
+        count++;
+        x++;
+        y++;
+    }
+
+    return count;
+}
---
+++ src/GoBangImpl.h
@@ -0,0 +1,77 @@
+﻿#pragma once
+#include <stdbool.h>
+#include <stdint.h>
+
+#define GOBANG_BOARD_GRID_MAX 15  // 棋盘行/列数
+
+// 棋子类型
+typedef enum _GOBANG_PIECE_TYPE
+{
+    GOBANG_PIECE_NONE = 0,
+    GOBANG_PIECE_WHITE,    // 白棋
+    GOBANG_PIECE_BLACK,    // 黑棋
+    GOBANG_PIECE_MAX
+} GOBANG_PIECE_TYPE;
+
+// 单格棋盘信息
+typedef struct _gobang_board_info_t
+{
+    bool used;               // 是否已被使用
+    int x;                   // 横坐标 列
+    int y;                   // 纵坐标 行
+    GOBANG_PIECE_TYPE type;  // 棋子类型
+} gobang_board_info_t;
+
+class CGoBangImpl
+{
+public:
+    CGoBangImpl() = default;
+    ~CGoBangImpl() = default;
+
+    // 初始化
+    bool init();
+
+    // 销毁
+    void uninit();
+
+    // 获取棋盘信息
+    bool getBoardInfo(gobang_board_info_t * board_info[GOBANG_BOARD_GRID_MAX]);
+
+    // 落子
+    bool pieceDown(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 判断棋子是否能下
+    bool checkPieceDown(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 判断是否结束
+    bool checkGameOver(int x, int y, GOBANG_PIECE_TYPE type);
+
+protected:
+    // 获取棋子上方同色棋子个数
+    uint16_t getTopPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子左上方同色棋子个数
+    uint16_t getTopLeftPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子右上方同色棋子个数
+    uint16_t getTopRightPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子左方同色棋子个数
+    uint16_t getLeftPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子右方同色棋子个数
+    uint16_t getRightPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子下方同色棋子个数
+    uint16_t getBottomPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子左下方同色棋子个数
+    uint16_t getBottomLeftPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+    // 获取棋子右下方同色棋子个数
+    uint16_t getBottomRightPiecesCount(int x, int y, GOBANG_PIECE_TYPE type);
+
+private:
+    // 棋盘信息
+    gobang_board_info_t _board_info[GOBANG_BOARD_GRID_MAX][GOBANG_BOARD_GRID_MAX];
+};
